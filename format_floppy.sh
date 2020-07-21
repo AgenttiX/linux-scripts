e405c44f5abe221e1afec8f27c04124ec00ceeeb
Mika Mäki <agentti002x@gmail.com>
2020-07-21 19:39:02 +0300

    Improve format_floppy.sh

--- format_floppy.sh
+++ format_floppy.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-echo "Shredding & formatting $1"
-badblocks -wsv "$1"
+
+if [ "$1" == "" ]; then
+  echo "Please add a device name as an argument."
+  exit 1;
+fi
+
+echo "Formatting $1"
 if [[ "$1" =~ ^/dev/sd.* ]]; then
-  echo "This is a USB device. Formatting with ubiformat."
+  echo "This is a USB device. Formatting with ufiformat."
+  ufiformat --inquire "$1"
   ufiformat --verify "$1"
 elif [[ "$1" == ^/dev/fd.* ]]; then
   echo "This is a true floppy drive. Formatting with fdformat."
@@ -10,3 +16,9 @@ elif [[ "$1" == ^/dev/fd.* ]]; then
 else
   echo "Unknown device type!"
 fi
+
+echo "Testing $1"
+badblocks -wsv "$1"
+
+echo "Creating file system"
+mkfs.vfat -c -v "$1"
