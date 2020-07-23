4d7fc98fb4d989ac227f4a1011a166eb155fc77d
Mika Mäki (TAU) <mika.maki@tuni.fi>
2020-07-23 18:44:21 +0300

    Add LUKS TPM scripts

---
+++ luks-tpm/setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+apt-get update
+apt-get install -y tpm2-initramfs-tool
+read -s -p "Please input the current LUKS passphrase:" luks_passphrase
+echo
+tpm2-initramfs-tool seal --data "$luks_passphrase"
+echo
+cp ./tpm2-initramfs-tool /etc/initramfs-tools/hooks/
+cp ./tpm2-initramfs-script /bin/
+chown root:root /etc/initramfs-tools/hooks/tpm2-initramfs-tool
+chown root:root /usr/bin/tpm2-initramfs-script
+chmod 755 /etc/initramfs-tools/hooks/tpm2-initramfs-tool
+chmod 755 /usr/bin/tpm2-initramfs-script
+echo "Now edit /etc/crypttab and append \",keyscript=/usr/bin/tpm2-initramfs-script\" to the root disk configuration"
