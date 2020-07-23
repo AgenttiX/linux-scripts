2742a3b3cb8ce42cf310093c70176788bebc6151
Mika Mäki (TAU) <mika.maki@tuni.fi>
2020-07-23 18:50:03 +0300

    Add config validation to the LUKS TPM setup script

--- luks-tpm/setup.sh
+++ luks-tpm/setup.sh
@@ -12,4 +12,7 @@ chown root:root /etc/initramfs-tools/hooks/tpm2-initramfs-tool
 chown root:root /usr/bin/tpm2-initramfs-script
 chmod 755 /etc/initramfs-tools/hooks/tpm2-initramfs-tool
 chmod 755 /usr/bin/tpm2-initramfs-script
-echo "Now edit /etc/crypttab and append \",keyscript=/usr/bin/tpm2-initramfs-script\" to the root disk configuration"
+echo "Checking that the script works"
+/usr/bin/tpm2-initramfs-script
+echo "Now edit /etc/crypttab and append \",keyscript=/usr/bin/tpm2-initramfs-script\" to the root disk configuration."
+echo "Then run \"update-initramfs -u\" to apply the configuration."
