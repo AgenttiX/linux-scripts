fa7171b33d228a42e680a64118e73d3e50a28ad7
Mika Mäki <agentti002x@gmail.com>
2020-12-25 19:54:09 +0200

    Add iperf installation and startup to setup script

--- iperf/setup.sh
+++ iperf/setup.sh
@@ -5,7 +5,13 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
+apt-get update
+apt-get install iperf iperf3
 cp ./iperf.service /etc/systemd/system/iperf.service
 cp ./iperf3.service /etc/systemd/system/iperf3.service
 chmod 644 /etc/systemd/system/iperf.service
 chmod 644 /etc/systemd/system/iperf3.service
+systemctl start iperf
+systemctl start iperf3
+systemctl status iperf
+systemctl status iperf3
