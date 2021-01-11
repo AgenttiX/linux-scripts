92ffe1be2ac2ebf94ee3e8fbdfed1983eefdc13a
Mika Mäki <agentti002x@gmail.com>
2021-01-12 01:42:21 +0200

    Add a missing semicolon to librenms.sh

--- librenms/librenms.sh
+++ librenms/librenms.sh
@@ -99,7 +99,7 @@ select yn in "Yes" "No"; do
         No ) INSTALL_SMART=0 ;;
     esac
 done
-if [ $INSTALL_SMART -eq 1 ] then
+if [ $INSTALL_SMART -eq 1 ]; then
     apt-get install -y smartmontools
     wget https://github.com/librenms/librenms-agent/raw/master/snmp/smart -O /etc/snmp/smart
     chown root:root /etc/snmp/smart
