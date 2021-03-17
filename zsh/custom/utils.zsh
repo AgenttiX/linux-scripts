dacbdaebe368ebf3056938e88d9ac7f00cc72f17
Mika Mäki <agentti002x@gmail.com>
2021-03-17 18:54:06 +0200

    Add rocm-smi alias

--- zsh/custom/utils.zsh
+++ zsh/custom/utils.zsh
@@ -61,3 +61,7 @@ alias sagdu="sudo apt-get update && sudo apt-get dist-upgrade"
 
 # Print most recently modified files in current directory. It takes no arguments
 alias vikat="find ${1} -type f | xargs stat --format '%Y :%y: %n' 2>/dev/null | sort -nr | cut -d: -f2,3,5 | head"
+
+if ! command -v rocm-smi &> /dev/null && [ -f "/opt/rocm/bin/rocm-smi" ]; then
+  alias rocm-smi="/opt/rocm/bin/rocm-smi"
+fi
