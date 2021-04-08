61824bbc2403f02555410dd52c017639328c5182
Mika Mäki <agentti002x@gmail.com>
2021-04-08 21:27:58 +0300

    Fix Pythia removal script

--- physics/remove_pythia.sh
+++ physics/remove_pythia.sh
@@ -5,9 +5,11 @@ if [ "$EUID" -ne 0 ]; then
   exit
 fi
 
-rm /usr/local/bin/pythia8-config
-rm /usr/local/share/Pythia8 -r
-rm /usr/local/lib/_pythia8.so
-rm /usr/local/lib/libpythia8.a
-rm /usr/local/lib/libpythia8*.so
-rm /usr/local/lib/pythia8.*
+rm -rf /usr/local/include/Pythia8
+rm -rf /usr/local/include/Pythia8Plugins
+rm -rf /usr/local/share/Pythia8
+rm -f /usr/local/bin/pythia8-config
+rm -f /usr/local/lib/_pythia8.so
+rm -f /usr/local/lib/libpythia8.a
+rm -f /usr/local/lib/libpythia8*.so
+rm -f /usr/local/lib/pythia8.*
