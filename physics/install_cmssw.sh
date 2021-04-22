927033905b6e21385fb73bc9e4f3e2300b3acebe
Mika Mäki <agentti002x@gmail.com>
2021-04-23 02:12:48 +0300

    Update CMSSW installation script

--- physics/install_cmssw.sh
+++ physics/install_cmssw.sh
@@ -8,6 +8,8 @@
 # "Error while seeding rpm database with system packages."
 # Proper Ubuntu installation instructions are behind a CERN login
 # https://twiki.cern.ch/twiki/bin/view/CMSPublic/SDTCMSSW_aptinstaller#Ubuntu
+# Some more Ubuntu instructions
+# https://twiki.cern.ch/twiki/bin/view/Main/CMSSWUbuntuInstallation
 
 # Attempting to install CMSSW as root will result in
 # "*** CMS SOFTWARE INSTALLATION ABORTED ***"
@@ -22,7 +24,10 @@ sudo mkdir -p $VO_CMS_SW_DIR
 sudo chown -R $USER:$USER $VO_CMS_SW_DIR
 wget -O "${VO_CMS_SW_DIR}/bootstrap.sh" "http://cmsrep.cern.ch/cmssw/repos/bootstrap.sh"
 export SCRAM_ARCH=slc7_amd64_gcc820
+mkdir -p "/tmp/${USER}"
 
 # This is "sh" in the documentation, but that would result in the necessary command "source" being undefined
 # https://askubuntu.com/questions/504546/error-message-source-not-found-when-running-a-script
-bash -x "${VO_CMS_SW_DIR}/bootstrap.sh" setup -path "${VO_CMS_SW_DIR}" -arch "${SCRAM_ARCH}" >& "${VO_CMS_SW_DIR}/bootstrap_${SCRAM_ARCH}.log"
+# bash -x "${VO_CMS_SW_DIR}/bootstrap.sh" setup -path "${VO_CMS_SW_DIR}" -arch "${SCRAM_ARCH}" >& "${VO_CMS_SW_DIR}/bootstrap_${SCRAM_ARCH}.log"
+bash -x $VO_CMS_SW_DIR/bootstrap.sh setup -path $VO_CMS_SW_DIR -arch $SCRAM_ARCH -unsupported_distribution_hack -v >& $VO_CMS_SW_DIR/bootstrap_$SCRAM_ARCH.log
+source $VO_CMS_SW_DIR/$SCRAM_ARCH/external/apt/*/etc/profile.d/init.sh
