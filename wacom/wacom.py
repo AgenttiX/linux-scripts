406aafbc9412f5a2a277f5c77541fc73b68c94ef
Mika Mäki <agentti002x@gmail.com>
2021-06-02 11:50:27 +0300

    Fix compatibility of the Wacom script with different tablet modes

--- wacom/wacom.py
+++ wacom/wacom.py
@@ -50,10 +50,43 @@ class Rotation(str, enum.Enum):
     CCW = "ccw"
 
 
+@enum.unique
+class DevType(str, enum.Enum):
+    CURSOR = "CURSOR"
+    ERASER = "ERASER"
+    PAD = "PAD"
+    STYLUS = "STYLUS"
+
+
 class Device:
     """An individual xsetwacom device such as the stylus or pad"""
-    def __init__(self, name: str):
+    def __init__(
+            self,
+            name: str,
+            dev_id: int,
+            dev_type: DevType):
         self.name = name
+        self.dev_id = dev_id
+        self.dev_type = dev_type
+
+    @classmethod
+    def create_by_names(cls, names: tp.List[str]) -> "Device":
+        devices = cls.get_devices()
+        for name in names:
+            found = next((dev for dev in devices if dev.name == name), None)
+            if found is not None:
+                return found
+        raise IndexError(f"Device not found with the names: {names}")
+
+    @staticmethod
+    def get_devices() -> tp.List["Device"]:
+        ret_code, lst = utils.run([XSETWACOM, "--list", "devices"], get_output=True, print_output=False)
+        props = [[elem.strip() for elem in line.split("\t")] for line in lst]
+        return [Device(
+            name=line[0],
+            dev_id=line[1],
+            dev_type=DevType(line[2].split(":")[1].strip())
+        ) for line in props]
 
     def set_value(self, *args):
         utils.run([XSETWACOM, "set", self.name] + [str(arg) for arg in args])
@@ -143,15 +176,18 @@ def get_display_info(name: str = ":0") -> tp.Dict[str, tp.Tuple[int, int]]:
 def main(use_big: bool = True):
     logger.info("Running Wacom script")
     utils.alert_if_root(fail=True)
-    stylus = Device("Wacom Intuos BT M Pen stylus")
-    pad = Device("Wacom Intuos BT M Pad pad")
+    stylus = Device.create_by_names([
+        "Wacom Intuos BT M Pen stylus",
+        "Wacom Co.,Ltd. Intuos BT M stylus"
+    ])
+    # pad = Device("Wacom Intuos BT M Pad pad")
 
     # stylus.print_all_settings()
     stylus.set_suppress(False)
     stylus.set_raw_sample(True)
-    pad.set_raw_sample(True)
+    # pad.set_raw_sample(True)
     stylus.set_threshold(128)
-    pad.set_threshold(128)
+    # pad.set_threshold(128)
 
     monitors = get_display_info()
     monitor_names = list(monitors.keys())
