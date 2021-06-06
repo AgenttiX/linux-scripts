de830bb9c36420f1b304ee8c7530d2b349d2492c
Mika Mäki <agentti002x@gmail.com>
2021-06-07 00:36:01 +0300

    Enable Android platform tools upgrades by removing old versions

--- android/download_tools.sh
+++ android/download_tools.sh
@@ -3,5 +3,7 @@
 FILENAME="platform-tools-latest-linux.zip"
 URL="https://dl.google.com/android/repository/${FILENAME}"
 
+rm -f $FILENAME
 wget $URL -O $FILENAME
+rm -rf ./platform-tools
 unzip $FILENAME
