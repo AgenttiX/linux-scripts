151bf5ec518762004e4bf6052f70a0d463b1c68d
Mika Mäki <agentti002x@gmail.com>
2021-09-15 15:54:35 +0300

    Add CUDA installation script

---
+++ drivers/install_cuda.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+# CUDA installer
+# From
+# https://developer.nvidia.com/cuda-downloads?target_os=Linux&target_arch=x86_64&Distribution=Ubuntu&target_version=20.04&target_type=deb_network
+
+# You can find cuDNN at
+# https://developer.nvidia.com/rdp/cudnn-download
+
+if ! [ "$EUID" -eq 0 ]; then
+  echo "This script should be run as root."
+  exit 1
+fi
+
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
+mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
+apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/7fa2af80.pub
+add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
+apt-get update
+apt-get install cuda
