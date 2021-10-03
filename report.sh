9a15a08b756ff0cc117807a48d034992d019e701
Mika Mäki <agentti002x@gmail.com>
2021-10-03 15:42:05 +0300

    Improve the reporting script

--- report.sh
+++ report.sh
@@ -1,34 +1,90 @@
 #!/bin/bash -e
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-DIR="${SCRIPT_DIR}/logs/report"
-
-if [ -z "$DIR" ]; then
-  echo "Could not configure directory variable: $DIR"
+if [ "${EUID}" -eq 0 ]; then
+  echo "This script should not be run as root."
   exit 1
 fi
 
-mkdir -p "$DIR"
-if [ "$(ls -A $DIR)" ]; then
-  rm "${DIR:?}"/*
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+DIR="${SCRIPT_DIR}/logs/report"
+
+if [ -z "${DIR}" ]; then
+  echo "Could not configure directory variable: ${DIR}"
+  exit 1
 fi
 
-hostname | tee -a "$DIR/basic.txt"
-uname -a | tee -a "$DIR/basic.txt"
+# Install dependencies
+sudo apt-get update
+sudo apt-get install clinfo dmidecode lshw lsscsi p7zip vainfo vdpauinfo
 
-lsblk -a > "$DIR/lsblk.txt"
-lscpu > "$DIR/lscpu.txt"
-lshw -html > "$DIR/lshw.html"
-lspci > "$DIR/lspci.txt"
-lsusb > "$DIR/lsusb.txt"
+mkdir -p "${DIR}"
+# Remove old results
+if [ "$(ls -A $DIR)" ]; then
+  rm -r "${DIR:?}"/*
+fi
+mkdir -p "${DIR}/hdparm" "${DIR}/smartctl"
 
-vainfo &> "$DIR/vainfo.txt"
-vdpauinfo &> "$DIR/vdpauinfo.txt"
+# Basic info
+echo -n "Hostname: "
+hostname |& tee -a "${DIR}/basic.txt"
+echo -n "Uname: "
+uname -a |& tee -a "${DIR}/basic.txt"
+echo "HDDs" |& tee -a "${DIR}/basic.txt"
+smartctl --scan |& tee -a "${DIR}/basic.txt"
 
-xinput list &> "$DIR/xinput.txt"
-xrandr &> "$DIR/xrandr.txt"
+# Non-root info
+lsblk -a &> "${DIR}/lsblk.txt"
+lscpu &> "${DIR}/lscpu.txt"
 
-# TODO
-# dmidecode
-# hdparm
-# lsscsi
+lspci &> "${DIR}/lspci.txt"
+lsscsi &> "${DIR}/lsscsi.txt"
+lsusb &> "${DIR}/lsusb.txt"
+
+vainfo &> "${DIR}/vainfo.txt"
+vdpauinfo &> "${DIR}/vdpauinfo.txt"
+
+xinput list &> "${DIR}/xinput.txt"
+xrandr &> "${DIR}/xrandr.txt"
+
+# Root info
+sudo dmidecode &> "${DIR}/dmidecode.txt"
+sudo lshw -html > "${DIR}/lshw.html"
+
+# Storage devices
+mapfile -t SMARTCTL_SCAN < <(smartctl --scan)
+for LINE in "${SMARTCTL_SCAN[@]}"; do
+  IFS=', ' read -r -a ARR <<< "${LINE}"
+  DISK="${ARR[0]}"
+  DISK_NAME="$(basename "${DISK}")"
+  sudo hdparm -I "${DISK}" &> "${DIR}/hdparm/${DISK_NAME}"
+  sudo smartctl --all "${DISK}" &> "${DIR}/smartctl/${DISK_NAME}"
+done
+
+# GPU info
+if command -v clinfo &> /dev/null; then
+  clinfo &> "${DIR}/clinfo.txt"
+else
+  echo "nvidia-smi was not found. This system probably doesn't have Nvidia GPUs installed."
+fi
+
+if command -v nvidia-smi &> /dev/null; then
+  nvidia-smi &> "${DIR}/nvidia-smi.txt"
+else
+  echo "nvidia-smi was not found. This system probably doesn't have Nvidia GPUs installed."
+fi
+
+if command -v rocminfo &> /dev/null; then
+  rocminfo &> "${DIR}/rocminfo.txt"
+else
+  echo "rocminfo was not yet found."
+fi
+
+if command -v rocm-smi &> /dev/null; then
+  rocm-smi &> "${DIR}/rocm-smi.txt"
+else
+  echo "rocm-smi was not yet found."
+fi
+
+# Packaging
+7zr a -mx=9 "${DIR}_$(date '+%Y-%m-%d_%H-%M-%S').7z" "${DIR}"
+echo "The report is ready."
