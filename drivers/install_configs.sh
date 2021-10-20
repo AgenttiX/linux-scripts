1651676b327bede32ecd8d7fcb77f5a8b8698ba3
Mika Mäki <agentti002x@gmail.com>
2021-10-20 22:24:09 +0300

    Add script for configuring a multi-monitor high fps setup

---
+++ drivers/install_configs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+
+if [ "${EUID}" -ne 0 ]; then
+  echo "This script should be run as root."
+  exit 1
+fi
+
+XORG_CONF_DIR="/etc/X11/xorg.conf.d/"
+cp "./xorg.conf.d/"*.conf "${XORG_CONF_DIR}"
+chmod 644 "${XORG_CONF_DIR}"/*.conf
+
+cp "./50-refresh-rate.conf" "/etc/environment.d/"
+chmod 644 "/etc/environment.d/50-refresh-rate.conf"
