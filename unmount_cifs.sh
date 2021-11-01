470c9eff51e6dcfe183c8ad1f81525b63024c21d
Mika Mäki <agentti002x@gmail.com>
2021-11-01 11:26:52 +0200

    Add unmounting script for network shares

---
+++ unmount_cifs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+# https://www.commandlinefu.com/commands/view/10958/unmount-all-cifs-drives
+
+if [ "${EUID}" -ne 0 ]; then
+   echo "This script should be run as root."
+   exit 1
+fi
+
+for DRIVE in $(mount -lt cifs | sed 's/.*on \(\/.\+\) type.*/\1/') do
+  echo "Unmounting ${DRIVE}"
+  umount "${DRIVE}"
+  umount -l "${DRIVE}"
+done
+echo "All CIFS network shares are now unmounted."
