35594e9ad117de9745d44b1a3ca4a5a41d0bb6dc
Mika Mäki <agentti002x@gmail.com>
2021-11-20 23:30:51 +0200

    Add a script for fixing boot issues

---
+++ fix_boot_issues.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+
+if [ "${EUID}" -ne 0 ]; then
+  echo "This script should be run as root."
+  exit 1
+fi
+
+apt-get update
+apt-get --fix-broken install
+update-initramfs -u
+update-grub
