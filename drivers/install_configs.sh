58a4aab8e050abb33b542611908b675f087ef906
Mika Mäki <agentti002x@gmail.com>
2021-12-10 20:24:53 +0200

    Add info prints for driver config installation

--- drivers/install_configs.sh
+++ drivers/install_configs.sh
@@ -5,9 +5,13 @@ if [ "${EUID}" -ne 0 ]; then
   exit 1
 fi
 
+echo "Installing configs."
+
 XORG_CONF_DIR="/etc/X11/xorg.conf.d/"
 cp "./xorg.conf.d/"*.conf "${XORG_CONF_DIR}"
 chmod 644 "${XORG_CONF_DIR}"/*.conf
 
 cp "./50-refresh-rate.conf" "/etc/environment.d/"
 chmod 644 "/etc/environment.d/50-refresh-rate.conf"
+
+echo "Configs installed."
