5329f4f80a1e86992ed5696548179537b694b24f
Mika Mäki <agentti002x@gmail.com>
2021-12-18 11:40:50 +0200

    Add teamredminer script

---
+++ crypto/teamredminer.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/sh -e
+# TODO: Work in progress
+
+. ./ethminer_config.sh
+
+MINER="./teamredminer/teamredminer"
+$MINER --list_devices
+
+$MINER \
+  --algo=ethash \
+  --log_file=autotune_full_log.txt \
+  --url="stratum+ssl://${SERVER}:${PORT}" \
+  --user="${ADDRESS}" \
+  --eth_stratum_mode="ethproxy"
+  # --auto_tune=SCAN \
+  # --auto_tune_runs=6 \
