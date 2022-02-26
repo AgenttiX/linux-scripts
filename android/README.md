5a3cbeeedd608097cad0764786aff73158ad63cd
Mika Mäki <agentti002x@gmail.com>
2022-02-27 00:51:41 +0200

    Improve Android flashing instructions and scripts

--- android/README.md
+++ android/README.md
@@ -31,16 +31,47 @@ This folder contains some tools for working with Android devices.
 - [Magisk](https://github.com/topjohnwu/Magisk) (preferred)
 - [SuperSU](https://supersuroot.org/)
 
-## Installation order
-- Upgrade of stock ROM for firmware upgrades (if possible)
-  - The stock ROM may contain newer firmware for the modem, bluetooth etc., which improves security and reliability
-  - Please ensure, though, that the newer stock ROM doesn't lock the bootloader or introduce other additional restrictions
-- Recovery (TWRP)
-- Firmware from the stock ROM (if not already upgraded)
-  - This can be done with the scripts of this repository
+## Installation steps
+These instructions look very complicated, but they are actually quite straightforward,
+unless you have a device with the A/B partition scheme or without a separate recovery partition.
+
+- Upgrade of stock ROM for firmware upgrades (if doing a clean install and if it's possible)
+  - The stock ROM may contain newer firmware for the modem, bluetooth etc., which improves security and reliability.
+  - Please ensure, though, that the newer stock ROM doesn't lock the bootloader or introduce other additional restrictions.
+- Install fastboot-installable official firmware from the stock ROM (if not already upgraded)
+  - This can be done with the scripts of this repository.
+- Select the boot slot you want to install to
+  - `sudo fastboot --set-active=a` (or b)
+- Boot to recovery (TWRP)
+  - Try these in this order until one of them works
+    - Samsung devices
+       1) `sudo heimdall flash --RECOVERY <TWRP image>.img`
+       2) Use Odin
+    - Fastboot devices
+      1) `sudo fastboot install recovery <TWRP image>.img` and boot from the on-device menu
+      2) `sudo fastboot boot <TWRP image>.img`
+      3) `sudo fastboot install boot <TWRP image>.img` and boot from the on-device menu
+  - If TWRP gets stuck at the logo, try an older version.
+- ADB-installable firmware from the stock ROM (if doing a clean install)
+  - This can be done with the scripts of this repository.
 - ROM (LineageOS)
+  - Select the boot slot you want to install to.
+  - Wipe the system and cache partitions
+    - On devices without a separate recovery partition TWRP cannot be booted without a working OS installation,
+      so don't reboot after wiping system before LineageOS is installed.
+  - Select the boot slot you don't want to install to.
+  - Install LineageOS (This will install to the slot that is not active at the moment.)
+  - Select "reboot to recovery"
+- If the device boots to the LineageOS recovery, ensure that the active slot is the slot you want to install to.
+  - Then you have to use `adb sideload <file>.zip` to install the rest.
+  - Install regardless of the signature warnings. They merely warn that the additional zips aren't provided by LineageOS.
 - Google Apps
 - Kernel (if available)
+- TWRP zip (on devices without a recovery partition)
+  - This will install to both slots.
 - Root (Magisk)
+  - The Magisk .apk has to be renamed to a .zip for it to work.
+  - TWRP wiped this, so you have to do this for both slots when upgrading.
+- Reboot to system
 
-([XDA discussion](https://forum.xda-developers.com/t/what-is-the-proper-order-of-flashing-rom-kernel-root-gapps-and-anti-throttle.3651521/))
+([XDA discussion of proper installation order](https://forum.xda-developers.com/t/what-is-the-proper-order-of-flashing-rom-kernel-root-gapps-and-anti-throttle.3651521/))
--- android/flash_oneplus6.sh
+++ android/flash_oneplus6.sh
@@ -3,8 +3,9 @@
 # This script is for flashing the partitions manually using images extracted
 # from an official OxygenOS zip, which you can get here:
 # https://www.oneplus.com/global/support/softwareupgrade
-# Use this to extract the files from the OTA zip:
+# Use this payload dumper to extract the files from the OTA zip:
 # https://github.com/vm03/payload_dumper
+# It supports running directly from a pre-built Docker container.
 
 # After you have run this script, you may want to run the additional
 # script for flashing the critical partitions.
@@ -21,48 +22,63 @@ if [ "${EUID}" -ne 0 ]; then
   exit 1
 fi
 
+# Enable factory reset if doing a clean install!
+FACTORY_RESET=false
+FLASH_A=true
+FLASH_B=true
+
 # https://stackoverflow.com/a/246128/
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 FASTBOOT="$SCRIPT_DIR/platform-tools/fastboot"
 
 # Factory reset
-$FASTBOOT -w
+if [ "${FACTORY_RESET}" = true ]; then
+  echo "Factory resetting the device"
+  $FASTBOOT -w
+fi
 
 # The partition list below is based on these instructions
 # https://forum.xda-developers.com/t/rom-stock-fastboot-op6-stock-fastboot-roms-for-oneplus-6.3796665/
 # https://www.droidwin.com/restore-oneplus-6-stock-via-fastboot-commands/
-$FASTBOOT flash aop_a aop.img
-$FASTBOOT flash aop_b aop.img
-$FASTBOOT flash bluetooth_a bluetooth.img
-$FASTBOOT flash bluetooth_b bluetooth.img
-$FASTBOOT flash boot_a boot.img
-$FASTBOOT flash boot_b boot.img
-$FASTBOOT flash dsp_a dsp.img
-$FASTBOOT flash dsp_b dsp.img
-$FASTBOOT flash dtbo_a dtbo.img
-$FASTBOOT flash dtbo_b dtbo.img
-$FASTBOOT flash fw_4j1ed_a fw_4j1ed.img
-$FASTBOOT flash fw_4j1ed_b fw_4j1ed.img
-$FASTBOOT flash fw_4u1ea_a fw_4u1ea.img
-$FASTBOOT flash fw_4u1ea_b fw_4u1ea.img
-$FASTBOOT flash modem_a modem.img
-$FASTBOOT flash modem_b modem.img
-$FASTBOOT flash oem_stanvbk oem_stanvbk.img
-$FASTBOOT flash qupfw_a qupfw.img
-$FASTBOOT flash qupfw_b qupfw.img
-$FASTBOOT flash storsec_a storsec.img
-$FASTBOOT flash storsec_b storsec.img
-$FASTBOOT flash system_a system.img
-$FASTBOOT flash system_b system.img
-$FASTBOOT flash vbmeta_a vbmeta.img
-$FASTBOOT flash vbmeta_b vbmeta.img
-$FASTBOOT flash vendor_a vendor.img
-$FASTBOOT flash vendor_b vendor.img
-$FASTBOOT flash LOGO_a LOGO.img
-$FASTBOOT flash LOGO_b LOGO.img
+if [ "${FLASH_A}" = true ]; then
+  echo "Flashing slot A"
+  $FASTBOOT flash aop_a aop.img
+  $FASTBOOT flash bluetooth_a bluetooth.img
+  $FASTBOOT flash boot_a boot.img
+  $FASTBOOT flash dsp_a dsp.img
+  $FASTBOOT flash dtbo_a dtbo.img
+  $FASTBOOT flash fw_4j1ed_a fw_4j1ed.img
+  $FASTBOOT flash fw_4u1ea_a fw_4u1ea.img
+  $FASTBOOT flash modem_a modem.img
+  $FASTBOOT flash oem_stanvbk oem_stanvbk.img
+  $FASTBOOT flash qupfw_a qupfw.img
+  $FASTBOOT flash storsec_a storsec.img
+  $FASTBOOT flash system_a system.img
+  $FASTBOOT flash vbmeta_a vbmeta.img
+  $FASTBOOT flash vendor_a vendor.img
+  $FASTBOOT flash LOGO_a LOGO.img
+fi
+if [ "${FLASH_B}" = true ]; then
+  echo "Flashing slot B"
+  $FASTBOOT flash aop_b aop.img
+  $FASTBOOT flash bluetooth_b bluetooth.img
+  $FASTBOOT flash boot_b boot.img
+  $FASTBOOT flash dsp_b dsp.img
+  $FASTBOOT flash dtbo_b dtbo.img
+  $FASTBOOT flash fw_4j1ed_b fw_4j1ed.img
+  $FASTBOOT flash fw_4u1ea_b fw_4u1ea.img
+  $FASTBOOT flash modem_b modem.img
+  $FASTBOOT flash qupfw_b qupfw.img
+  $FASTBOOT flash storsec_b storsec.img
+  $FASTBOOT flash system_b system.img
+  $FASTBOOT flash vbmeta_b vbmeta.img
+  $FASTBOOT flash vendor_b vendor.img
+  $FASTBOOT flash LOGO_b LOGO.img
+fi
 
 # This would overwrite sensor calibrations and some other unique data
 # $FASTBOOT flash persist persist.img
 
 # Rebooting back to the bootloader just in case to ensure that all changes have been applied
+echo "Rebooting the device to the bootloader"
 $FASTBOOT reboot bootloader
