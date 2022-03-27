024ff8a26a254df24727c5ecb4c6f3d515403709
Mika Mäki <agentti002x@gmail.com>
2022-03-27 17:51:43 +0300

    Change startup script install location

--- startup/agx-startup-enabler.sh
+++ startup/agx-startup-enabler.sh
@@ -1,11 +1,20 @@
-#!/usr/bin/env sh
+#!/bin/bash -e
+
+if [ "${EUID}" -ne 0 ]; then
+  echo "This script should be run as root."
+  exit 1
+fi
+
 echo "Installing the agx-startup script"
 cp ./agx-startup.service /etc/systemd/system/
-cp ./agx_startup.py /usr/local/bin/
+mkdir -p /usr/local/bin/agx
+cp ./agx_startup.py /usr/local/bin/agx/
 chown root:root /etc/systemd/system/agx-startup.service
-chown root:root /usr/local/bin/agx_startup.py
+chown root:root /usr/local/bin/agx/agx_startup.py
 chmod 644 /etc/systemd/system/agx-startup.service
-chmod 755 /usr/local/bin/agx_startup.py
+chmod 755 /usr/local/bin/agx/agx_startup.py
 systemctl daemon-reload
 systemctl enable agx-startup.service
+# Remove old script versions
+rm -f /usr/local/bin/agx_startup.py
 echo "Installation ready"
