967b2feffb43f905cf4332754183345032bbb542
Mika Mäki <agentti002x@gmail.com>
2022-08-22 16:42:16 +0300

    Add login banner script

---
+++ security/enable_banner.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -e
+
+if [ "${EUID}" -ne 0 ]; then
+  echo "This script should be run as root."
+  exit 1
+fi
+
+# From:
+# https://help.ubuntu.com/community/StricterDefaults#SSH_Welcome_Banner
+# https://www.cryptsus.com/blog/how-to-secure-your-ssh-server-with-public-key-elliptic-curve-ed25519-crypto.html
+cp ./issue.net /etc/issue.net
+chmod 744 /etc/issue.net
+sed -i '/^#Banner .*/a Banner \/etc\/issue.net' /etc/ssh/sshd_config
+echo "You have to restart the sshd service with \"sudo systemctl restart sshd.service\""
