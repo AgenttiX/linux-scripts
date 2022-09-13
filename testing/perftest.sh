0852539be419a10ef5402784b413ac9251c4201d
Mika Mäki <agentti002x@gmail.com>
2022-09-13 11:57:03 +0300

    Improve perftest.sh

--- testing/perftest.sh
+++ testing/perftest.sh
@@ -6,19 +6,9 @@ if [ "${EUID}" -eq 0 ]; then
 fi
 
 # This already runs apt-get update
-source ./report.sh
+source ./report.sh --no-report
 
-if ! which speedtest > /dev/null; then
-  curl -s https://install.speedtest.net/app/cli/install.deb.sh | sudo bash
-fi
-sudo apt-get install sysbench speedtest stress-ng wget
-
-./geekbench/download_geekbench.sh
-
-speedtest |& tee "${DIR}/speedtest.txt"
-# These are managed by PTS
-# 7z b -mmt1 |& tee "${DIR}/7z_single_thread.txt"
-# 7z b |& tee "${DIR}/7z.txt"
+# Function definitions
 
 function run_geekbench_gpu() {
   VERSION=$1
@@ -33,12 +23,16 @@ function run_geekbench_gpu() {
   $GEEKBENCH --compute CUDA --save "${DIR}/geekbench${VERSION}_result_cuda.txt" |& tee -a "${OUTPUT}"
   $GEEKBENCH --compute OpenCL --save "${DIR}/geekbench${VERSION}_result_opencl.txt"|& tee -a "${OUTPUT}"
 }
-run_geekbench_gpu "5"
-run_geekbench_gpu "4"
 
-# TODO: Passmark
+# Installers
 
-# TODO: PTS
+if ! which speedtest > /dev/null; then
+  curl -s https://install.speedtest.net/app/cli/install.deb.sh | sudo bash
+fi
+sudo apt-get install sysbench speedtest stress-ng wget
+
+./geekbench/download_geekbench.sh
+
 if ! which phoronix-test-suite > /dev/null; then
   FILENAME="phoronix-test-suite_10.8.1_all.deb"
   wget "https://phoronix-test-suite.com/releases/repo/pts.debian/files/${FILENAME}" -O "${FILENAME}"
@@ -48,5 +42,22 @@ if ! which phoronix-test-suite > /dev/null; then
   phoronix-test-suite batch-setup
 fi
 phoronix-test-suite openbenchmarking-refresh
+
+# Tests
+
+speedtest |& tee "${DIR}/speedtest.txt"
+
+# These are managed by PTS
+# 7z b -mmt1 |& tee "${DIR}/7z_single_thread.txt"
+# 7z b |& tee "${DIR}/7z.txt"
+
+if which cryptsetup > /dev/null; then
+  cryptsetup benchmark > "${DIR}/cryptsetup.txt"
+fi
+
+run_geekbench_gpu "5"
+run_geekbench_gpu "4"
+
+# TODO: Passmark
 
 ./pts/run-tests.sh
