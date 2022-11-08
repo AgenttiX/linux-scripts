f4e63910ff3c9f57c67bc7bb73c59c675c5760a5
Mika Mäki <agentti002x@gmail.com>
2022-11-08 17:42:25 +0200

    Update Phoronix Test Suite version

--- testing/perftest.sh
+++ testing/perftest.sh
@@ -34,7 +34,7 @@ sudo apt-get install sysbench speedtest stress-ng wget
 ./geekbench/download_geekbench.sh
 
 if ! which phoronix-test-suite > /dev/null; then
-  FILENAME="phoronix-test-suite_10.8.1_all.deb"
+  FILENAME="phoronix-test-suite_10.8.4_all.deb"
   wget "https://phoronix-test-suite.com/releases/repo/pts.debian/files/${FILENAME}" -O "${FILENAME}"
   sudo dpkg -i "${FILENAME}"
   rm "${FILENAME}"
