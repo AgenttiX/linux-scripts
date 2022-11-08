4b0225d21d7d26775ca40e3888b0cfa9e287b980
Mika Mäki <agentti002x@gmail.com>
2022-11-08 17:00:00 +0200

    Update Speedtest installer url

--- testing/perftest.sh
+++ testing/perftest.sh
@@ -27,7 +27,7 @@ function run_geekbench_gpu() {
 # Installers
 
 if ! which speedtest > /dev/null; then
-  curl -s https://install.speedtest.net/app/cli/install.deb.sh | sudo bash
+  curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | sudo bash
 fi
 sudo apt-get install sysbench speedtest stress-ng wget
 
