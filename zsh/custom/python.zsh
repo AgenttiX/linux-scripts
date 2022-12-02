0e4843802912c8dd9aa26e53545b58c9a8086b0a
Mika Mäki <agentti002x@gmail.com>
2022-12-03 00:24:58 +0200

    Replace Anaconda with Miniconda

--- zsh/custom/python.zsh
+++ zsh/custom/python.zsh
@@ -4,14 +4,14 @@
 # "conda init zsh" in a shell that has conda enabled (such as bash)
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('${HOME}/anaconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+__conda_setup="$('${HOME}/miniconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
 if [ $? -eq 0 ]; then
     eval "$__conda_setup"
 else
-    if [ -f "${HOME}/anaconda3/etc/profile.d/conda.sh" ]; then
-        . "${HOME}/anaconda3/etc/profile.d/conda.sh"
+    if [ -f "${HOME}/miniconda33/etc/profile.d/conda.sh" ]; then
+        . "${HOME}/miniconda3/etc/profile.d/conda.sh"
     else
-        export PATH="${HOME}/anaconda3/bin:$PATH"
+        export PATH="${HOME}/miniconda3/bin:$PATH"
     fi
 fi
 unset __conda_setup
