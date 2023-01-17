d41d95add64ba482c1c407e33eaec1dae6493821
Mika Mäki <agentti002x@gmail.com>
2023-01-17 22:22:41 +0200

    Move Factorio firewall script to factorio-server repo

--- games/factorio_firewall.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-
-if [ "${EUID}" -ne 0 ]; then
-   echo "This script should be run as root."
-   exit 1
-fi
-
-ufw allow 34197/udp comment Factorio
-ufw allow 27015/tcp comment Factorio
