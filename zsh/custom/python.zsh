07e73ca153fa608aa0bb5bcfce69730c68783596
Mika Mäki <agentti002x@gmail.com>
2023-04-25 15:27:35 +0300

    Fix Miniconda path

--- zsh/custom/python.zsh
+++ zsh/custom/python.zsh
@@ -8,7 +8,7 @@ __conda_setup="$('${HOME}/miniconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)
 if [ $? -eq 0 ]; then
     eval "$__conda_setup"
 else
-    if [ -f "${HOME}/miniconda33/etc/profile.d/conda.sh" ]; then
+    if [ -f "${HOME}/miniconda3/etc/profile.d/conda.sh" ]; then
         . "${HOME}/miniconda3/etc/profile.d/conda.sh"
     else
         export PATH="${HOME}/miniconda3/bin:$PATH"
