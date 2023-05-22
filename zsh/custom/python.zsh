c6f7e0f4ffd7621aaa74f65611ccfb9d3ac1acb6
Mika Mäki <agentti002x@gmail.com>
2023-05-22 11:12:05 +0300

    Fix conda init

--- zsh/custom/python.zsh
+++ zsh/custom/python.zsh
@@ -4,7 +4,7 @@
 # "conda init zsh" in a shell that has conda enabled (such as bash)
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('${HOME}/miniconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+__conda_setup="$("${HOME}/miniconda3/bin/conda" 'shell.zsh' 'hook' 2> /dev/null)"
 if [ $? -eq 0 ]; then
     eval "$__conda_setup"
 else
