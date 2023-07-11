a33cbd105737bf3490f39c2946f1e53ca1c20a49
Mika Mäki <agentti002x@gmail.com>
2023-07-12 01:44:46 +0300

    Improve OnePlus 6 scripts

--- android/flash_oneplus6.sh
+++ android/flash_oneplus6.sh
@@ -1,9 +1,13 @@
-#!/usr/bin/bash -e
+#!/usr/bin/env bash
+set -e
 
 # This script is for flashing the partitions manually using images extracted
 # from an official OxygenOS zip, which you can get here:
 # https://www.oneplus.com/global/support/softwareupgrade
-# Use this payload dumper to extract the files from the OTA zip:
+
+# You can use this payload dumper to extract the files from the OTA zip:
+# https://github.com/tobyxdd/android-ota-payload-extractor
+# Or alternatively this:
 # https://github.com/vm03/payload_dumper
 # It supports running directly from a pre-built Docker container.
 
@@ -23,62 +27,128 @@ if [ "${EUID}" -ne 0 ]; then
 fi
 
 # Enable factory reset if doing a clean install!
-FACTORY_RESET=false
+FACTORY_RESET=true
 FLASH_A=true
 FLASH_B=true
 
 # https://stackoverflow.com/a/246128/
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-FASTBOOT="$SCRIPT_DIR/platform-tools/fastboot"
+REPO_DIR="$(dirname "${SCRIPT_DIR}")"
+FASTBOOT="${SCRIPT_DIR}/platform-tools/fastboot"
+CWD="${PWD}"
+
+TIMESTAMP="$(date +%Y-%m-%d_%H-%M-%S)"
+LOG_PATH="${REPO_DIR}/logs/flash_oneplus6_${TIMESTAMP}.txt"
+
+echo "Starting OnePlus 6 fastboot flash script." |& tee "${LOG_PATH}"
+
+# Directory checks
+
+OTA_DIR="${CWD}/OTA"
+if [ -d "${OTA_DIR}" ]; then
+  echo "OTA directory found."
+else
+  echo "OTA directory was not found. Please put the img files to the subdirectory \"OTA\"."
+  exit 1
+fi
+
+LINEAGEOS_DIR="${CWD}/LineageOS"
+if [ -d "${LINEAGEOS_DIR}" ]; then
+  echo "LineageOS directory found."
+else
+  echo "LineageOS directory was not found. Please put the img files and signatures to the subdirectory \"LineageOS\"."
+  exit 1
+fi
+
+TWRP_DIR="${CWD}/TWRP"
+if [ -d "${TWRP_DIR}" ]; then
+  echo "TWRP directory found."
+else
+  echo "TWRP directory was not found. Please put the TWRP img file and signatures to the subdirectory \"TWRP\"."
+fi
+
+# File checks
+
+cd "${LINEAGEOS_DIR}"
+sha256sum -c "./imgs.sha256"
+# "${CWD}"
+
+cd "${TWRP_DIR}"
+TWRP_PATTERN="${TWRP_DIR}/*.img"
+TWRP_FILES=($TWRP_PATTERN)
+TWRP_IMG="${TWRP_FILES[0]}"
+sha256sum -c "${TWRP_IMG}.sha256"
+wget "https://eu.dl.twrp.me/public.asc" -O "${TWRP_DIR}/public.asc"
+gpg --import "${TWRP_DIR}/public.asc"
+gpg --verify "${TWRP_IMG}.asc" "${TWRP_IMG}"
+cd "${CWD}"
 
 # Factory reset
 if [ "${FACTORY_RESET}" = true ]; then
-  echo "Factory resetting the device"
-  $FASTBOOT -w
+  echo "Factory resetting the device" |& tee -a "${LOG_PATH}"
+  $FASTBOOT -w |& tee -a "${LOG_PATH}"
 fi
 
 # The partition list below is based on these instructions
 # https://forum.xda-developers.com/t/rom-stock-fastboot-op6-stock-fastboot-roms-for-oneplus-6.3796665/
 # https://www.droidwin.com/restore-oneplus-6-stock-via-fastboot-commands/
 if [ "${FLASH_A}" = true ]; then
-  echo "Flashing slot A"
-  $FASTBOOT flash aop_a aop.img
-  $FASTBOOT flash bluetooth_a bluetooth.img
-  $FASTBOOT flash boot_a boot.img
-  $FASTBOOT flash dsp_a dsp.img
-  $FASTBOOT flash dtbo_a dtbo.img
-  $FASTBOOT flash fw_4j1ed_a fw_4j1ed.img
-  $FASTBOOT flash fw_4u1ea_a fw_4u1ea.img
-  $FASTBOOT flash modem_a modem.img
-  $FASTBOOT flash oem_stanvbk oem_stanvbk.img
-  $FASTBOOT flash qupfw_a qupfw.img
-  $FASTBOOT flash storsec_a storsec.img
-  $FASTBOOT flash system_a system.img
-  $FASTBOOT flash vbmeta_a vbmeta.img
-  $FASTBOOT flash vendor_a vendor.img
-  $FASTBOOT flash LOGO_a LOGO.img
+  echo "Flashing OTA images to slot A" |& tee -a "${LOG_PATH}"
+  $FASTBOOT flash aop_a "${OTA_DIR}/aop.img" |& tee -a "${LOG_PATH}"
+  $FASTBOOT flash bluetooth_a "${OTA_DIR}/bluetooth.img" |& tee -a "${LOG_PATH}"
+  $FASTBOOT flash boot_a "${OTA_DIR}/boot.img" |& tee -a "${LOG_PATH}"
+  $FASTBOOT flash dsp_a "${OTA_DIR}/dsp.img" |& tee -a "${LOG_PATH}"
+  $FASTBOOT flash dtbo_a "${OTA_DIR}/dtbo.img" |& tee -a "${LOG_PATH}"
+  $FASTBOOT flash fw_4j1ed_a "${OTA_DIR}/fw_4j1ed.img" |& tee -a "${LOG_PATH}"
+  $FASTBOOT flash fw_4u1ea_a "${OTA_DIR}/fw_4u1ea.img" |& tee -a "${LOG_PATH}"
+  $FASTBOOT flash modem_a "${OTA_DIR}/modem.img" |& tee -a "${LOG_PATH}"
+  $FASTBOOT flash oem_stanvbk "${OTA_DIR}/oem_stanvbk.img" |& tee -a "${LOG_PATH}"
+  $FASTBOOT flash qupfw_a "${OTA_DIR}/qupfw.img" |& tee -a "${LOG_PATH}"
+  $FASTBOOT flash storsec_a "${OTA_DIR}/storsec.img" |& tee -a "${LOG_PATH}"
+  $FASTBOOT flash system_a "${OTA_DIR}/system.img" |& tee -a "${LOG_PATH}"
+  $FASTBOOT flash vbmeta_a "${OTA_DIR}/vbmeta.img" |& tee -a "${LOG_PATH}"
+  $FASTBOOT flash vendor_a "${OTA_DIR}/vendor.img" |& tee -a "${LOG_PATH}"
+  $FASTBOOT flash LOGO_a "${OTA_DIR}/LOGO.img" |& tee -a "${LOG_PATH}"
 fi
 if [ "${FLASH_B}" = true ]; then
-  echo "Flashing slot B"
-  $FASTBOOT flash aop_b aop.img
-  $FASTBOOT flash bluetooth_b bluetooth.img
-  $FASTBOOT flash boot_b boot.img
-  $FASTBOOT flash dsp_b dsp.img
-  $FASTBOOT flash dtbo_b dtbo.img
-  $FASTBOOT flash fw_4j1ed_b fw_4j1ed.img
-  $FASTBOOT flash fw_4u1ea_b fw_4u1ea.img
-  $FASTBOOT flash modem_b modem.img
-  $FASTBOOT flash qupfw_b qupfw.img
-  $FASTBOOT flash storsec_b storsec.img
-  $FASTBOOT flash system_b system.img
-  $FASTBOOT flash vbmeta_b vbmeta.img
-  $FASTBOOT flash vendor_b vendor.img
-  $FASTBOOT flash LOGO_b LOGO.img
+  echo "Flashing OTA images to slot B" |& tee -a "${LOG_PATH}"
+  $FASTBOOT flash aop_b "${OTA_DIR}/aop.img" |& tee -a "${LOG_PATH}"
+  $FASTBOOT flash bluetooth_b "${OTA_DIR}/bluetooth.img" |& tee -a "${LOG_PATH}"
+  $FASTBOOT flash boot_b "${OTA_DIR}/boot.img" |& tee -a "${LOG_PATH}"
+  $FASTBOOT flash dsp_b "${OTA_DIR}/dsp.img" |& tee -a "${LOG_PATH}"
+  $FASTBOOT flash dtbo_b "${OTA_DIR}/dtbo.img" |& tee -a "${LOG_PATH}"
+  $FASTBOOT flash fw_4j1ed_b "${OTA_DIR}/fw_4j1ed.img" |& tee -a "${LOG_PATH}"
+  $FASTBOOT flash fw_4u1ea_b "${OTA_DIR}/fw_4u1ea.img" |& tee -a "${LOG_PATH}"
+  $FASTBOOT flash modem_b "${OTA_DIR}/modem.img" |& tee -a "${LOG_PATH}"
+  $FASTBOOT flash qupfw_b "${OTA_DIR}/qupfw.img" |& tee -a "${LOG_PATH}"
+  $FASTBOOT flash storsec_b "${OTA_DIR}/storsec.img" |& tee -a "${LOG_PATH}"
+  $FASTBOOT flash system_b "${OTA_DIR}/system.img" |& tee -a "${LOG_PATH}"
+  $FASTBOOT flash vbmeta_b "${OTA_DIR}/vbmeta.img" |& tee -a "${LOG_PATH}"
+  $FASTBOOT flash vendor_b "${OTA_DIR}/vendor.img" |& tee -a "${LOG_PATH}"
+  $FASTBOOT flash LOGO_b "${OTA_DIR}/LOGO.img" |& tee -a "${LOG_PATH}"
+fi
+
+if [ "${FLASH_A}" = true ]; then
+  echo "Flashing LineageOS to slot A"
+  $FASTBOOT flash boot_a "${LINEAGEOS_DIR}/boot.img" |& tee -a "${LOG_PATH}"
+  $FASTBOOT flash dtbo_a "${LINEAGEOS_DIR}/dtbo.img" |& tee -a "${LOG_PATH}"
+  $FASTBOOT flash vbmeta_a "${LINEAGEOS_DIR}/vbmeta.img" |& tee -a "${LOG_PATH}"
+fi
+if [ "${FLASH_B}" = true ]; then
+  echo "Flashing LineageOS to slot B"
+  $FASTBOOT flash boot_b "${LINEAGEOS_DIR}/boot.img" |& tee -a "${LOG_PATH}"
+  $FASTBOOT flash dtbo_b "${LINEAGEOS_DIR}/dtbo.img" |& tee -a "${LOG_PATH}"
+  $FASTBOOT flash vbmeta_b "${LINEAGEOS_DIR}/vbmeta.img" |& tee -a "${LOG_PATH}"
 fi
 
 # This would overwrite sensor calibrations and some other unique data
-# $FASTBOOT flash persist persist.img
+# $FASTBOOT flash persist persist.img |& tee -a "${LOG_PATH}"
 
 # Rebooting back to the bootloader just in case to ensure that all changes have been applied
-echo "Rebooting the device to the bootloader"
-$FASTBOOT reboot bootloader
+echo "Rebooting the device to the bootloader" |& tee -a "${LOG_PATH}"
+$FASTBOOT reboot bootloader |& tee -a "${LOG_PATH}"
+
+echo "Booting the device to TWRP" |& tee -a "${LOG_PATH}"
+$FASTBOOT boot "${TWRP_IMG}" |& tee -a "${LOG_PATH}"
+
+echo "Flashing script ready." |& tee -a "${LOG_PATH}"
--- android/flash_oneplus6_criticals.sh
+++ android/flash_oneplus6_criticals.sh
@@ -1,4 +1,5 @@
-#!/usr/bin/bash -e
+#!/usr/bin/env bash
+set -e
 
 # This script is for flashing the additional critical partitions with TWRP.
 # This should not be done, unless you want to completely wipe
@@ -11,30 +12,38 @@ fi
 
 # https://stackoverflow.com/a/246128/
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+REPO_DIR="$(dirname "${SCRIPT_DIR}")"
 ADB="$SCRIPT_DIR/platform-tools/adb"
 
+TIMESTAMP="$(date +%Y-%m-%d_%H-%M-%S)"
+LOG_PATH="${REPO_DIR}/logs/flash_oneplus6_criticals_${TIMESTAMP}.txt"
+
+echo "Starting OnePlus 6 critical partition flasher script." |& tee "${LOG_PATH}"
+
 # https://forum.xda-developers.com/t/rom-stock-fastboot-op6-stock-fastboot-roms-for-oneplus-6.3796665/
-$ADB push abl.img /sdcard/abl.img
-$ADB shell dd if=/sdcard/abl.img of=/dev/block/bootdevice/by-name/abl_a
-$ADB shell dd if=/sdcard/abl.img of=/dev/block/bootdevice/by-name/abl_b
-$ADB push cmnlib.img /sdcard/cmnlib.img
-$ADB shell dd if=/sdcard/cmnlib.img of=/dev/block/bootdevice/by-name/cmnlib_a
-$ADB shell dd if=/sdcard/cmnlib.img of=/dev/block/bootdevice/by-name/cmnlib_b
-$ADB push cmnlib64.img /sdcard/cmnlib64.img
-$ADB shell dd if=/sdcard/cmnlib64.img of=/dev/block/bootdevice/by-name/cmnlib64_a
-$ADB shell dd if=/sdcard/cmnlib64.img of=/dev/block/bootdevice/by-name/cmnlib64_b
-$ADB push devcfg.img /sdcard/devcfg.img
-$ADB shell dd if=/sdcard/devcfg.img of=/dev/block/bootdevice/by-name/devcfg_a
-$ADB shell dd if=/sdcard/devcfg.img of=/dev/block/bootdevice/by-name/devcfg_b
-$ADB push hyp.img /sdcard/hyp.img
-$ADB shell dd if=/sdcard/hyp.img of=/dev/block/bootdevice/by-name/hyp_a
-$ADB shell dd if=/sdcard/hyp.img of=/dev/block/bootdevice/by-name/hyp_b
-$ADB push keymaster.img /sdcard/keymaster.img
-$ADB shell dd if=/sdcard/keymaster.img of=/dev/block/bootdevice/by-name/keymaster_a
-$ADB shell dd if=/sdcard/keymaster.img of=/dev/block/bootdevice/by-name/keymaster_b
-$ADB push xbl.img /sdcard/xbl.img
-$ADB shell dd if=/sdcard/xbl.img of=/dev/block/bootdevice/by-name/xbl_a
-$ADB shell dd if=/sdcard/xbl.img of=/dev/block/bootdevice/by-name/xbl_b
-$ADB push xbl_config.img /sdcard/xbl_config.img
-$ADB shell dd if=/sdcard/xbl_config.img of=/dev/block/bootdevice/by-name/xbl_config_a
-$ADB shell dd if=/sdcard/xbl_config.img of=/dev/block/bootdevice/by-name/xbl_config_b
+$ADB push abl.img /sdcard/abl.img |& tee -a "${LOG_PATH}"
+$ADB shell dd if=/sdcard/abl.img of=/dev/block/bootdevice/by-name/abl_a |& tee -a "${LOG_PATH}"
+$ADB shell dd if=/sdcard/abl.img of=/dev/block/bootdevice/by-name/abl_b |& tee -a "${LOG_PATH}"
+$ADB push cmnlib.img /sdcard/cmnlib.img |& tee -a "${LOG_PATH}"
+$ADB shell dd if=/sdcard/cmnlib.img of=/dev/block/bootdevice/by-name/cmnlib_a |& tee -a "${LOG_PATH}"
+$ADB shell dd if=/sdcard/cmnlib.img of=/dev/block/bootdevice/by-name/cmnlib_b |& tee -a "${LOG_PATH}"
+$ADB push cmnlib64.img /sdcard/cmnlib64.img |& tee -a "${LOG_PATH}"
+$ADB shell dd if=/sdcard/cmnlib64.img of=/dev/block/bootdevice/by-name/cmnlib64_a |& tee -a "${LOG_PATH}"
+$ADB shell dd if=/sdcard/cmnlib64.img of=/dev/block/bootdevice/by-name/cmnlib64_b |& tee -a "${LOG_PATH}"
+$ADB push devcfg.img /sdcard/devcfg.img |& tee -a "${LOG_PATH}"
+$ADB shell dd if=/sdcard/devcfg.img of=/dev/block/bootdevice/by-name/devcfg_a |& tee -a "${LOG_PATH}"
+$ADB shell dd if=/sdcard/devcfg.img of=/dev/block/bootdevice/by-name/devcfg_b |& tee -a "${LOG_PATH}"
+$ADB push hyp.img /sdcard/hyp.img |& tee -a "${LOG_PATH}"
+$ADB shell dd if=/sdcard/hyp.img of=/dev/block/bootdevice/by-name/hyp_a |& tee -a "${LOG_PATH}"
+$ADB shell dd if=/sdcard/hyp.img of=/dev/block/bootdevice/by-name/hyp_b |& tee -a "${LOG_PATH}"
+$ADB push keymaster.img /sdcard/keymaster.img |& tee -a "${LOG_PATH}"
+$ADB shell dd if=/sdcard/keymaster.img of=/dev/block/bootdevice/by-name/keymaster_a |& tee -a "${LOG_PATH}"
+$ADB shell dd if=/sdcard/keymaster.img of=/dev/block/bootdevice/by-name/keymaster_b |& tee -a "${LOG_PATH}"
+$ADB push xbl.img /sdcard/xbl.img |& tee -a "${LOG_PATH}"
+$ADB shell dd if=/sdcard/xbl.img of=/dev/block/bootdevice/by-name/xbl_a |& tee -a "${LOG_PATH}"
+$ADB shell dd if=/sdcard/xbl.img of=/dev/block/bootdevice/by-name/xbl_b |& tee -a "${LOG_PATH}"
+$ADB push xbl_config.img /sdcard/xbl_config.img |& tee -a "${LOG_PATH}"
+$ADB shell dd if=/sdcard/xbl_config.img of=/dev/block/bootdevice/by-name/xbl_config_a |& tee -a "${LOG_PATH}"
+$ADB shell dd if=/sdcard/xbl_config.img of=/dev/block/bootdevice/by-name/xbl_config_b |& tee -a "${LOG_PATH}"
+
+echo "OnePlus 6 critical partition flasher script ready." |& tee -a "${LOG_PATH}"
