b2320f630d973e39adf21cda48c2d956ff1ffa45
Mika Mäki <agentti002x@gmail.com>
2023-07-31 16:01:13 +0300

    Fix bugs in rclone_kapsi.sh

--- backup/rclone_kapsi.sh
+++ backup/rclone_kapsi.sh
@@ -6,11 +6,14 @@ set -e
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 REPO_DIR="$(dirname "${SCRIPT_DIR}")"
 TIMESTAMP="$(date +%Y-%m-%d_%H-%M-%S)"
-LOG_PATH="${REPO_DIR}/logs/rsync_kapsi_${TIMESTAMP}.txt"
+LOG_PATH="${REPO_DIR}/logs/rclone_kapsi_${TIMESTAMP}.txt"
 
+# Warning: the --delete-excluded flag will delete files on the target that are not part of the backup.
+# First run the backup without --delete-excluded to see that it goes to the correct empty folder,
+# and doesn't mess with other files on the target machine!
 rclone sync \
   --progress \
   --exclude-from="${SCRIPT_DIR}/rsync_exclude.txt" \
+  --delete-excluded \
   /mnt/files \
   kapsi-crypt:/files |& tee -a "${LOG_PATH}"
-# --delete-excluded
