2d9867ac36ce6b5419e6eed44bb5c004d72d164c
Mika Mäki <agentti002x@gmail.com>
2023-10-11 16:34:33 +0300

    Improve VSCodium scripts

--- vscodium/run_remote.sh
+++ vscodium/run_remote.sh
@@ -3,7 +3,11 @@ set -e
 
 # From https://github.com/xaberus/vscode-remote-oss
 
-export CONNECTION_TOKEN="wGyLXg0Z6NTbsTMd"
+if [ ! -f "${HOME}/.gitconfig" ]; then
+    echo "~/.gitconfig was not found. Please create it before running this script again."
+    exit 1
+fi
+export CONNECTION_TOKEN="$(sha256sum "${HOME}/.gitconfig")"
 export REMOTE_PORT=11111
 
 ~/.vscodium-server/bin/current/bin/codium-server \
