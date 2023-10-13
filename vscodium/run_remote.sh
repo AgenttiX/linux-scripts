cd24c08dc0da83d894a24ce6981a0a7ca9971821
Mika Mäki <agentti002x@gmail.com>
2023-10-13 17:57:27 +0300

    Fix VSCodium connection token

--- vscodium/run_remote.sh
+++ vscodium/run_remote.sh
@@ -1,13 +1,15 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 set -e
 
 # From https://github.com/xaberus/vscode-remote-oss
 
 if [ ! -f "${HOME}/.gitconfig" ]; then
+    # shellcheck disable=SC2088
     echo "~/.gitconfig was not found. Please create it before running this script again."
     exit 1
 fi
-export CONNECTION_TOKEN="$(sha256sum "${HOME}/.gitconfig")"
+CONNECTION_TOKEN="$(sha256sum "${HOME}/.gitconfig" | cut -f 1 -d " ")"
+export CONNECTION_TOKEN
 export REMOTE_PORT=11111
 
 ~/.vscodium-server/bin/current/bin/codium-server \
