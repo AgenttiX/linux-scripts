0f29e0325badc77482ec6c1a6a59bbb875f33d64
Mika Mäki <agentti002x@gmail.com>
2023-10-30 10:22:02 +0200

    Fix an extra backslash in the CUDA script

--- drivers/install_cuda.sh
+++ drivers/install_cuda.sh
@@ -28,7 +28,7 @@ dpkg -i cuda-keyring_1.1-1_all.deb
 curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg
 curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
   sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
-  sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list \
+  sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
 
 apt-get update
 apt-get install cuda nvidia-container-toolkit
