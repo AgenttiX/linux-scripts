14463f846e1c663b57b302c829976c0e63a0526b
Mika Mäki <agentti002x@gmail.com>
2023-11-29 15:56:26 +0200

    Add Xournal++ installer script and update its settings

---
+++ xournalpp/setup-xournalpp.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -e
+
+if [ "${EUID}" -eq 0 ]; then
+  echo "This script should not be run as root."
+  exit 1
+fi
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+CONFIG_DIR="${HOME}/.config/xournalpp"
+CONFIG_DIR2="${HOME}/.var/app/com.github.xournalpp.xournalpp/config/xournalpp"
+
+flatpak install flathub com.github.xournalpp.xournalpp
+
+# Backup old configs
+if [ -e "${CONFIG_DIR}" ] && [ ! -L "${CONFIG_DIR}" ]; then
+  mv "${CONFIG_DIR}" "${CONFIG_DIR}-old"
+fi
+if [ -e "${CONFIG_DIR2}" ] && [ ! -L "${CONFIG_DIR2}" ]; then
+  mv "${CONFIG_DIR2}" "${CONFIG_DIR2}-old"
+fi
+
+ln -f -s "${SCRIPT_DIR}" "${CONFIG_DIR}"
+ln -f -s "${SCRIPT_DIR}" "${CONFIG_DIR2}"
