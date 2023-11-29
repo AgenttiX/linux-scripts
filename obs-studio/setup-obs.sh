55cd1f60c34616fb97fd6fa9830eb223fe6c305f
Mika Mäki <agentti002x@gmail.com>
2023-11-29 20:52:09 +0200

    Update OBS script for Flatpak

--- obs-studio/setup-obs.sh
+++ obs-studio/setup-obs.sh
@@ -7,17 +7,27 @@ if [ "${EUID}" -eq 0 ]; then
 fi
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-CONFIG_PATH="${HOME}/.var/app/com.obsproject.Studio/config/obs-studio"
+CONFIG_PATH="${HOME}/.config/obs-studio"
+CONFIG_PATH_FLATPAK="${HOME}/.var/app/com.obsproject.Studio/config/obs-studio"
 
-flatpak install flathub com.obsproject.Studio
+sudo add-apt-repository ppa:obsproject/obs-studio
+
+# https://github.com/obsproject/obs-studio/wiki/install-instructions#prerequisites-for-all-versions
+sudo apt-get update
+sudo apt-get install ffmpeg obs-studio v4l2loopback-dkms
+
+# flatpak install flathub com.obsproject.Studio
 
 if [ -d "${CONFIG_PATH}" ] && [ ! -L "${CONFIG_PATH}" ]; then
-  echo "Backing up old config directory."
+  echo "Backing up old OBS config directory."
   mv "${CONFIG_PATH}" "${CONFIG_PATH}-old"
 fi
-# Non-flatpak config directory:
-# "${HOME}/.config/obs-studio"
+if [ -d "${CONFIG_PATH_FLATPAK}" ] && [ ! -L "${CONFIG_PATH_FLATPAK}" ]; then
+  echo "Backing up old OBS flatpak config directory."
+  mv "${CONFIG_PATH_FLATPAK}" "${CONFIG_PATH_FLATPAK}-old"
+fi
 
-echo "Creating symlink."
-ln -s "${SCRIPT_DIR}" "${CONFIG_PATH}"
+echo "Creating symlinks."
+ln -f -s "${SCRIPT_DIR}" "${CONFIG_PATH}"
+ln -f -s "${SCRIPT_DIR}" "${CONFIG_PATH_FLATPAK}"
 echo "Configuration ready."
