e579532a3e61e99fa294630fce8cde633b7a4947
Mika Mäki <agentti002x@gmail.com>
2024-11-03 16:34:59 +0200

    Add script for showing ECC memory info

---
+++ drivers/show_ecc_info.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+set -eu
+
+# https://www.setphaserstostun.org/posts/monitoring-ecc-memory-on-linux-with-rasdaemon/
+
+if ! command -v ras-mc-ctl 2>&1 >/dev/null; then
+  echo "Rasdaemon seems not to be installed. Installing."
+  apt-get update
+  apt-get install rasdaemon
+  systemctl enable rasdaemon
+  systemctl start rasdaemon
+fi
+
+# "To identify which DIMM slot corresponds to which EDAC path
+# you will have to reboot your system with only one DIMM inserted,
+# write down the name of the slot you insterted it in and then printing out the paths with ras-mc-ctl --error-count."
+ras-mc-ctl --print-labels
+
+ras-mc-ctl --error-count
