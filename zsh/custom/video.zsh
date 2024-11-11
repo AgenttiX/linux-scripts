10ec6c5ed9b9e52e3c6a8f38872ade0d6842eac5
Mika Mäki <agentti002x@gmail.com>
2024-11-12 00:11:09 +0200

    Add video rotation fixing script

--- zsh/custom/video.zsh
+++ zsh/custom/video.zsh
@@ -7,3 +7,19 @@ trim_video() {
   fi
   ffmpeg -i $1 -ss $2 -to $3 -c:v copy -c:a copy $4
 }
+
+fix_rotation() {
+  # Fix wrong rotation metadata in a video.
+  # Custom Android ROMs can sometimes give wrong orientation sensor data,
+  # resulting in wrong orientation metadata in video recordings.
+  if [ $# -lt 1 ]; then
+    echo "Usage: fix_rotation <source paths>"
+  fi
+  for ARG in "$@"; do
+    NAME="${ARG%.*}"
+    SUFFIX="${ARG##*.}"
+    OLD_NAME="${NAME}-old.${SUFFIX}"
+    mv "${ARG}" "${OLD_NAME}"
+    ffmpeg -display_rotation 0 -i "${OLD_NAME}" -c copy "${ARG}"
+  done
+}
