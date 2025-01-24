3c42bf7967d76e6fbee57a8d8f3ae30c8de5c5b1
Mika Mäki <agentti002x@gmail.com>
2025-01-24 11:44:09 +0200

    Add ClamTk, libreoffice-help-fi and git-gui to the installer script

--- apt/install_software.sh
+++ apt/install_software.sh
@@ -14,10 +14,11 @@ fi
 
 apt-get update
 apt-get install \
-  apt-transport-https autojump bleachbit build-essential ca-certificates cifs-utils cmake curl cutecom \
+  apt-transport-https autojump bleachbit build-essential ca-certificates cifs-utils clamtk cmake curl cutecom \
   docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin \
-  filelight filezilla gfortran gimp git gparted htop inkscape kde-config-flatpak keepassxc ktorrent \
-  libreoffice lm-sensors mumble network-manager-openvpn openssh-server optipng powertop python3-dev python3-venv \
+  filelight filezilla gfortran gimp git git-gui gparted htop inkscape kde-config-flatpak keepassxc ktorrent \
+  libreoffice libreoffice-help-fi lm-sensors mumble network-manager-openvpn openssh-server optipng powertop \
+  python3-dev python3-venv \
   s-tui signal-desktop stress synaptic texlive-full texmaker ufw vlc wget wireguard yt-dlp zsh
 
 if [ "$(hostnamectl chassis)" = "laptop" ]; then
