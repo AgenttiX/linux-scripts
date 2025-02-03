1546ab7826191642ca5c77ddc3c7b5c050851f94
Mika Mäki <agentti002x@gmail.com>
2025-02-04 01:02:12 +0200

    Update RPCS3 configs

--- games/rpcs3/install_rpcs3.sh
+++ games/rpcs3/install_rpcs3.sh
@@ -7,16 +7,20 @@ if [ "${EUID}" -eq 0 ]; then
 fi
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-sudo apt-get update
-sudo apt-get install wget
-wget --content-disposition https://rpcs3.net/latest-appimage
-chmod +x ./rpcs3*.AppImage
+# sudo apt-get update
+# sudo apt-get install wget
+# wget --content-disposition https://rpcs3.net/latest-appimage
+# chmod +x ./rpcs3*.AppImage
+flatpak install flathub net.rpcs3.RPCS3 com.github.tchx84.Flatseal
 
-CONFIG_PATH="${HOME}/.config/rpcs3"
+# CONFIG_PATH="${HOME}/.config/rpcs3"
+CONFIG_PATH="${HOME}/.var/app/net.rpcs3.RPCS3/config/rpcs3"
 if ! [ -L "$CONFIG_PATH" ]; then
   mkdir -p "$(dirname "${CONFIG_PATH}")"
   if [ -d "$CONFIG_PATH" ]; then
-    rm -r "${CONFIG_PATH}"
+    mv "${CONFIG_PATH}" "${CONFIG_PATH}-old"
   fi
   ln -s "${SCRIPT_DIR}" "${CONFIG_PATH}"
 fi
+echo "Installation ready. Use Flatseal to allow RPCS3 access to the configuration directory:"
+echo "${SCRIPT_DIR}"
