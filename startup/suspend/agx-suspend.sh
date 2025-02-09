34998f3ce801a4a340768e00717b2aae505e526a
Mika Mäki <agentti002x@gmail.com>
2025-02-09 21:38:39 +0200

    Unmount CIFS shares on laptops when going to sleep

--- startup/suspend/agx-suspend.sh
+++ startup/suspend/agx-suspend.sh
@@ -1,8 +1,20 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 set -eu
 
 USERNAME="$(id -nu 1000)"
+
+if [ "${1}" = "pre" ]; then
+  # Laptops should umount all network shares when going to sleep
+  if [ "$(hostnamectl chassis)" = "laptop" ]; then
+    umount --all --types cifs
+  fi
 # After resume
-if [[ "${1}" == "post" ]]; then
-    sudo -u "${USERNAME}" -i "/home/${USERNAME}/Git/linux-scripts/startup/suspend/agx-resume-user.sh"
+elif [ "${1}" = "post" ]; then
+  USER_RESUME_SCRIPT="/home/${USERNAME}/Git/linux-scripts/startup/suspend/agx-resume-user.sh"
+  if [ -f "${USER_RESUME_SCRIPT}" ]; then
+    # Run as user
+    sudo -u "${USERNAME}" -i "${USER_RESUME_SCRIPT}"
+  else
+    echo "User resume script was not found at ${USER_RESUME_SCRIPT}"
+  fi
 fi
