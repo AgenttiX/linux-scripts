91d13c7456f2c4cdca464fcc6cf22faa0c7b2e88
Mika Mäki <agentti002x@gmail.com>
2025-02-09 21:37:50 +0200

    Disable plasmashell startup script

--- startup/suspend/agx-resume-user.sh
+++ startup/suspend/agx-resume-user.sh
@@ -2,9 +2,9 @@
 set -eu
 
 # This does not seem to fix the Plasma crash on Kubuntu 24.10
-if pgrep -x "plasmashell" > /dev/null; then
-  sleep 1
-  killall plasmashell -9
-  sleep 1
-  kstart plasmashell
-fi
+# if pgrep -x "plasmashell" > /dev/null; then
+#   sleep 1
+#   killall plasmashell -9
+#   sleep 1
+#   kstart plasmashell
+# fi
