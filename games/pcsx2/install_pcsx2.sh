d282d6052df89569a702854a802b979b135ef701
Mika Mäki <agentti002x@gmail.com>
2025-02-09 22:04:27 +0200

    Use Flatpak for PCSX2

--- games/pcsx2/install_pcsx2.sh
+++ games/pcsx2/install_pcsx2.sh
@@ -15,16 +15,18 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 # The daily repository has 64-bit builds.
 # However, the package "pcsx2" may still be 32-bit only.
-sudo add-apt-repository ppa:pcsx2-team/pcsx2-daily
+# sudo add-apt-repository ppa:pcsx2-team/pcsx2-daily
 
-sudo apt-get update
-sudo apt-get install pcsx2
+# sudo apt-get update
+# sudo apt-get install pcsx2
+flatpak install flathub net.pcsx2.PCSX2
 
-CONFIG_PATH="${HOME}/.config/PCSX2"
+# CONFIG_PATH="${HOME}/.config/PCSX2"
+CONFIG_PATH="${HOME}/.var/app/net.pcsx2.PCSX2/config/PCSX2"
 if ! [ -L "$CONFIG_PATH" ]; then
   mkdir -p "$(dirname "${CONFIG_PATH}")"
   if [ -d "$CONFIG_PATH" ]; then
-    rm -r "${CONFIG_PATH}"
+    mv "${CONFIG_PATH}" "${CONFIG_PATH}-old"
   fi
   ln -s "${SCRIPT_DIR}" "${CONFIG_PATH}"
 fi
