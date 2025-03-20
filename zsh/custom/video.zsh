cddd759cd3304743ac491909a134ad6949a0a74d
Mika Mäki <agentti002x@gmail.com>
2025-03-20 16:55:43 +0200

    Fix error handling in trim_video

--- zsh/custom/video.zsh
+++ zsh/custom/video.zsh
@@ -3,7 +3,7 @@
 trim_video() {
   if [ $# -ne 4 ]; then
     echo "Usage: trim_video <source path> <start time> <end time> <output path>"
-    exit 1
+    return 1
   fi
   ffmpeg -i $1 -ss $2 -to $3 -c:v copy -c:a copy $4
 }
