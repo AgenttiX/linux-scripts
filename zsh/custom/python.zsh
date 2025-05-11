faac5f6cbfa3ecdf4be605509327846ea094ca1f
Mika Mäki <mika.x.maki@helsinki.fi>
2025-05-11 17:27:33 +0300

    Use system Python even when Anaconda is installed

--- zsh/custom/python.zsh
+++ zsh/custom/python.zsh
@@ -4,18 +4,25 @@
 # "conda init zsh" in a shell that has conda enabled (such as bash)
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$("${HOME}/miniconda3/bin/conda" 'shell.zsh' 'hook' 2> /dev/null)"
-if [ $? -eq 0 ]; then
-    eval "$__conda_setup"
-else
-    if [ -f "${HOME}/miniconda3/etc/profile.d/conda.sh" ]; then
-        . "${HOME}/miniconda3/etc/profile.d/conda.sh"
-    else
-        export PATH="${HOME}/miniconda3/bin:$PATH"
-    fi
-fi
-unset __conda_setup
+# __conda_setup="$("${HOME}/miniconda3/bin/conda" 'shell.zsh' 'hook' 2> /dev/null)"
+# if [ $? -eq 0 ]; then
+#     eval "$__conda_setup"
+# else
+#     if [ -f "${HOME}/miniconda3/etc/profile.d/conda.sh" ]; then
+#         . "${HOME}/miniconda3/etc/profile.d/conda.sh"
+#     else
+#         export PATH="${HOME}/miniconda3/bin:$PATH"
+#     fi
+# fi
+# unset __conda_setup
 # <<< conda initialize <<<
+
+# Enable conda without setting it as the default Python
+CONDA_PATH="${HOME}/miniconda3/bin"
+if [ -d "${CONDA_PATH}" ]; then
+    export PATH="${PATH}:${CONDA_PATH}"
+fi
+unset CONDA_PATH
 
 # Fix Pythia 8 Python bindings
 if [ -d "/usr/local/share/Pythia8/" ]; then
