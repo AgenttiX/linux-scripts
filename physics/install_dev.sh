01c1656c92525778eb559a0ff9d59f28e4cf0056
Mika Mäki <agentti002x@gmail.com>
2025-07-07 01:07:00 +0300

    Remove hipcc from the dev installer script

--- physics/install_dev.sh
+++ physics/install_dev.sh
@@ -11,9 +11,13 @@ apt update
 # https://github.com/AdaptiveCpp/AdaptiveCpp
 # These seem to be also required for its cmake compilation: libedit-dev, libclang-rt-dev
 # libedit-dev seems to be required for cmake compilation of AdaptiveCPP
-apt install build-essential clang cmake hdf5-tools hipcc \
+apt install build-essential clang cmake hdf5-tools \
   libboost-dev libboost-context-dev libboost-fiber-dev libboost-test-dev \
   libedit-dev libgomp1 libhdf5-mpi-dev libomp-dev lld mpi-default-dev ninja-build pkg-config
 
+# These should perhaps be added to the list above: hipcc
+
+# Attempting to install multiple snaps simultaneously with classic containment results in this error:
+# error: cannot specify mode for multiple store snaps (only for one store snap or several local ones)
 snap install clion --classic
 snap install rustrover --classic
