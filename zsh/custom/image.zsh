9f7164855d24689e6d02b10943be587da0947a2e
Mika Mäki <agentti002x@gmail.com>
2025-07-06 17:11:56 +0300

    Update image script

--- zsh/custom/image.zsh
+++ zsh/custom/image.zsh
@@ -22,8 +22,8 @@ resize_image() {
   fi
 
   # Calculate the new dimensions while preserving the aspect ratio
-  new_larger_dimension=512
-  new_smaller_dimension=$((512 * smaller_dimension / larger_dimension))
+  new_larger_dimension=140
+  new_smaller_dimension=$((140 * smaller_dimension / larger_dimension))
   if [ "${dimensions[1]}" -ge "${dimensions[2]}" ]; then
     dimensions2="${new_larger_dimension}x${new_smaller_dimension}"
   else
@@ -31,7 +31,27 @@ resize_image() {
   fi
 
   # Resize the image using ImageMagick
-  convert "$input_image" -resize "$dimensions2" -format png "$output_image"
+  convert "$input_image" -resize "$dimensions2" -format bmp "$output_image"
 
   echo "Image resized and converted to PNG: $output_image"
+}
+
+resize_image_stick() {
+  if [ $# -ne 2 ]; then
+      echo "Usage: resize_image input_image output_image"
+      return 1
+  fi
+  input_image="$1"
+  output_image="$2"
+  dimensions=($(identify -format "%w %h" "$input_image"))
+
+  width="${dimensions[1]}"
+  height="${dimensions[2]}"
+  new_width=140
+  new_height=$((new_width * width / height + 1))
+
+  echo ${height} ${width}
+  echo ${new_height} ${new_width}
+  convert "${input_image}" -rotate 90 -resize "${new_width}x${new_height}" -format bmp "${output_image}"
+  echo "Image resized and converted for Timo's light stick: ${output_image}"
 }
