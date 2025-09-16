93e228b4759d41e4802db3d942e951b0889c2c94
Mika Mäki <mika.x.maki@helsinki.fi>
2025-09-16 16:58:49 +0300

    Fix small mistakes in scripts

--- backup/backup_dvd.sh
+++ backup/backup_dvd.sh
@@ -19,7 +19,7 @@ FILENAME=$1
 if [ $(command -v ddrescue &> /dev/null) -or $(command -v 7zr &> /dev/null) ]; then :; else
   echo "The necessary packages appear not to be installed. Installing."
   sudo apt update
-  sudo apt install gddrescue p7zip
+  sudo apt install gddrescue 7zip
 fi
 
 ddrescue -b 2048 -n -v "${DEVICE}" "${FILENAME}.iso" rescue.log
