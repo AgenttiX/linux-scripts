b4466ef250e4bed078a86a13fe460bffd0c4e7a4
Mika Mäki <mika.x.maki@helsinki.fi>
2025-10-15 18:49:45 +0200

    Replace screen with tmux in the installer script

--- apt/install_software.sh
+++ apt/install_software.sh
@@ -36,7 +36,8 @@ apt update
 echo "Constructing the list of apt packages to install."
 BASE_PACKAGES=(
   # Servers should have git-gui too for X11 forwarding.
-  "apt-transport-https" "ca-certificates" "git" "git-gui" "htop" "mosh" "openssh-server" "screen" "ufw"
+  # Screen has been replaced with tmux.
+  "apt-transport-https" "ca-certificates" "git" "git-gui" "htop" "mosh" "openssh-server" "tmux" "ufw"
 )
 DEV_PACKAGES=(
   "build-essential" "cloc" "cmake" "gcc-multilib" "g++-multilib" "gfortran"
