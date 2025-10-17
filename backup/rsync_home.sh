6b168e7ac8563237c9c8d328bb4b613be3ee1009
Mika Mäki <agentti002x@gmail.com>
2025-10-18 02:10:34 +0300

    Fix rsync target path

--- backup/rsync_home.sh
+++ backup/rsync_home.sh
@@ -5,9 +5,11 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 REPO_DIR="$(dirname "${SCRIPT_DIR}")"
 TIMESTAMP="$(date +%Y-%m-%d_%H-%M-%S)"
 LOG_PATH="${REPO_DIR}/logs/rsync_backup_${TIMESTAMP}.txt"
+HOSTNAME="$(hostname)"
+HOSTNAME_TRIMMED="${HOSTNAME%"-kubuntu"}"
 
 rsync \
   --archive --compress --delete --partial --progress --stats \
   --exclude-from="${SCRIPT_DIR}/rsync_exclude.txt" \
   "${HOME}" \
-  "${USER}@agx-file-backup:." |& tee -a "${LOG_PATH}"
+  "${USER}@agx-file-backup:/mnt/backup/${HOSTNAME_TRIMMED}/rsync/" |& tee -a "${LOG_PATH}"
