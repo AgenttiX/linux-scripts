27d4784012d19a427e5484c8a1ed745207a27407
Mika Mäki <agentti002x@gmail.com>
2026-01-07 02:16:42 +0200

    Implement stability testing

--- testing/stability_test.sh
+++ testing/stability_test.sh
@@ -3,7 +3,25 @@
 # - Fix memtester
 # - Add CPU stress testing
 
-# http://pyropus.ca/software/memtester/
-sudo apt install memtester
+sudo apt update
 
-memtester
+
+sudo apt install memtester stress-ng
+
+# flatpak install flathub com.geeks3d.furmark
+
+# NPROC=$(nproc)
+FREE_RAM="$(free -m | grep Mem | awk '{print $4}')"
+MEMTESTER_RAM=$((FREE_RAM / 2))
+MEMTESTER_ITERATIONS=10
+# echo "Available CPU cores: ${NPROC}"
+echo "Free RAM: ${FREE_RAM} MB, using for memtester: ${MEMTESTER_RAM} MB"
+
+echo "Running stress-ng."
+# Running as root enables e.g. process memory priority in low memory situations
+sudo stress-ng --matrix -1 --timeout 1m --metrics --perf --times --tz
+
+echo "Running memtester."
+# http://pyropus.ca/software/memtester/
+# Memtester should be run as root so that it can mlock the memory it tests.
+sudo memtester "${MEMTESTER_RAM}" "${MEMTESTER_ITERATIONS}"
