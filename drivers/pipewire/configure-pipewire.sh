04fbdca3bf48a9c0d85379d20f875e96024b71d7
Mika Mäki <mika.x.maki@helsinki.fi>
2026-01-12 09:46:19 +0200

    Fix missing Subnautica audio on agx-l14

--- drivers/pipewire/configure-pipewire.sh
+++ drivers/pipewire/configure-pipewire.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
 set -eu
 
-if [ "${EUID}" -ne 0 ]; then
-  echo "This script should be run as root."
+if [ "${EUID}" -eq 0 ]; then
+  echo "This script should not be run as root."
   exit 1
 fi
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+PIPEWIRE_CONF_DIR="/usr/share/pipewire"
 
-mkdir -p /etc/pipewire/pipewire.conf.d
-cp "${SCRIPT_DIR}/crackling.conf" "/etc/pipewire/pipewire.conf.d/crackling.conf"
+if [ ! -d "${PIPEWIRE_CONF_DIR}" ]; then
+  echo "PipeWire configuration directory was not found at \"${PIPEWIRE_CONF_DIR}\"."
+  exit 1
+fi
+
+sudo cp "${SCRIPT_DIR}/agx-custom.conf" "${PIPEWIRE_CONF_DIR}/agx-custom.conf"
 
 systemctl --user restart pipewire.service pipewire-pulse.service
