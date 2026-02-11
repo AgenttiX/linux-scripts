0cda0ef72dccf3ea0d3072471bc9d988637cf254
Mika Mäki <mika.x.maki@helsinki.fi>
2026-02-11 18:03:08 +0200

    Enable conda for zsh

--- zsh/custom/python.zsh
+++ zsh/custom/python.zsh
@@ -4,17 +4,17 @@
 # "conda init zsh" in a shell that has conda enabled (such as bash)
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-# __conda_setup="$("${HOME}/miniconda3/bin/conda" 'shell.zsh' 'hook' 2> /dev/null)"
-# if [ $? -eq 0 ]; then
-#     eval "$__conda_setup"
-# else
-#     if [ -f "${HOME}/miniconda3/etc/profile.d/conda.sh" ]; then
-#         . "${HOME}/miniconda3/etc/profile.d/conda.sh"
-#     else
-#         export PATH="${HOME}/miniconda3/bin:$PATH"
-#     fi
-# fi
-# unset __conda_setup
+__conda_setup="$("${HOME}/miniconda3/bin/conda" 'shell.zsh' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+   eval "$__conda_setup"
+else
+   if [ -f "${HOME}/miniconda3/etc/profile.d/conda.sh" ]; then
+       . "${HOME}/miniconda3/etc/profile.d/conda.sh"
+   else
+       export PATH="${HOME}/miniconda3/bin:$PATH"
+   fi
+fi
+unset __conda_setup
 # <<< conda initialize <<<
 
 # Enable conda without setting it as the default Python
