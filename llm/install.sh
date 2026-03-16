f5d195c9988e0e705aad55d51921ff0db095074c
Mika Mäki <agentti002x@gmail.com>
2026-03-17 00:47:36 +0200

    Improve LLM installer script

--- llm/install.sh
+++ llm/install.sh
@@ -10,28 +10,30 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 # Nix is required for llama.cpp
 if ! command -v nix &> /dev/null; then
-    echo "Installing Nix."
-    sh <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install) --daemon
+  echo "Installing Nix."
+  sh <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install) --daemon
+  echo "Please close and reopen your shell, and then rerun this script."
+  return 1
 fi
 
 if ! command -v ollama &> /dev/null; then
-    echo "Installing Ollama."
-    curl -fsSL https://ollama.com/install.sh | sh
+  echo "Installing Ollama."
+  curl -fsSL https://ollama.com/install.sh | sh
 fi
 
 if ! command -v llama-server &> /dev/null; then
-    echo "Installing llama.cpp."
-    nix profile install nixpkgs#llama-cpp --extra-experimental-features nix-command --extra-experimental-features flakes
+  echo "Installing llama.cpp."
+  nix profile install nixpkgs#llama-cpp --extra-experimental-features nix-command --extra-experimental-features flakes
 fi
 
 # if [ ! -f "${SCRIPT_DIR}/LM-Studio-"* ]; then
 #   echo "Downloading LM Studio."
-#   # This does not pick up the name properly.
+#   # This does not pick up the filename properly.
 #   curl -JLO "https://lmstudio.ai/download/latest/linux/x64" --output-dir "${SCRIPT_DIR}"
 #   chmod +x "${SCRIPT_DIR}/LM-Studio-"*
 # fi
 
-echo "Creating symlink."
+echo "Creating LM Studio shortcut symlink."
 ln -f -s "${SCRIPT_DIR}/lm-studio.desktop" "${HOME}/.local/share/applications/lm-studio.desktop"
 
 ollama --version
